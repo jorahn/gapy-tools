93be9034e4c06e017327c4475653b686dfd6793d
Jonathan Rahn <rahn.jonathan@gmail.com>
2021-09-07 10:37:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+from setuptools import setup, find_packages
 
+setup(
+    name='gapy',
+    version='0.1.0',
+    url='https://github.com/jorahn/gapy-tools.git',
+    author='Jonathan Rahn & Hoang Nguyen',
+    author_email='jonathan.rahn@42digital.de',
+    description='Google Analytics API Tools',
+    packages=find_packages(),
+    install_requires=['google-api-python-client >= 2.19.1', 'pandas >= 1.3.2'],
+)
