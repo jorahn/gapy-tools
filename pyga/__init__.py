0d52890f2eb776f9b077c0cd59dbec252e5e2d89
Jonathan Rahn <rahn.jonathan@gmail.com>
2021-09-07 11:59:54 +0200

    Update __init__.py

--- pyga/__init__.py
+++ pyga/__init__.py
@@ -21,4 +21,4 @@ class Client(object):
             }]
         }
         
-        return analytics.reports().batchGet(request).execute()
+        return self.client.reports().batchGet(request).execute()
