5d1455712a7ecb51a2af2b008cfc6aa06fc86f08
Jonathan Rahn <rahn.jonathan@gmail.com>
2021-09-07 11:42:29 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='gapy',
+    name='pyga',
     version='0.1.0',
     url='https://github.com/jorahn/gapy-tools.git',
     author='Jonathan Rahn & Hoang Nguyen',
